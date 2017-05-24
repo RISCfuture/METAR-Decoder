4373b1525e00dd0fbd44c01df72592b393ad33bb
Tim Morgan <tim@squareup.com>
2017-05-24 16:09:03 -0700

    Handle TSE01B02

--- METAR Decoder/Source/Models/Remark/ThunderstormBeginEndRemark.h
+++ METAR Decoder/Source/Models/Remark/ThunderstormBeginEndRemark.h
@@ -1,4 +1,8 @@
+#define THUNDERSTORM_BEGIN_END_EVENT_REGEX @"([BE])" REMARK_TIME_REGEX
+
 @interface ThunderstormBeginEndRemark : Remark
+
+@property (strong) NSMutableArray *events;
 
 @property (strong) NSDateComponents *begin;
 @property (strong) NSDateComponents *end;
--- METAR Decoder/Source/Models/Remark/ThunderstormBeginEndRemark.m
+++ METAR Decoder/Source/Models/Remark/ThunderstormBeginEndRemark.m
@@ -1,12 +1,36 @@
 #import "ThunderstormBeginEndRemark.h"
 
 // TSB0159E30
-static NSString *ThunderstormBeginEndRegex = @"\\bTSB" REMARK_TIME_REGEX @"E" REMARK_TIME_REGEX @"\\b\\s*";
+static NSString *ThunderstormBeginEndRegex = @"\\bTS" THUNDERSTORM_BEGIN_END_EVENT_REGEX THUNDERSTORM_BEGIN_END_EVENT_REGEX @"\\b\\s*";
+
+typedef enum _ThunderstormEventType {
+    ThunderstormEventTypeBegan = 0,
+    ThunderstormEventTypeEnded
+} ThunderstormEventType;
+
+@interface ThunderstormEvent: NSObject
+
+@property (assign) ThunderstormEventType type;
+@property (strong) NSDateComponents *date;
+
+@end
+
+@implementation ThunderstormEvent
+
+@synthesize type;
+@synthesize date;
+
+@end
+
+@interface ThunderstormBeginEndRemark (Private)
+
+- (ThunderstormEvent *) parseEventFromMatch:(NSTextCheckingResult *)match index:(NSUInteger)index inString:(NSString *)remarks;
+
+@end
 
 @implementation ThunderstormBeginEndRemark
 
-@synthesize begin;
-@synthesize end;
+@synthesize events;
 
 + (void) load {
     [Remark registerSubclass:self];
@@ -14,12 +38,16 @@ static NSString *ThunderstormBeginEndRegex = @"\\bTSB" REMARK_TIME_REGEX @"E" RE
 
 - (id) initFromRemarks:(NSMutableString *)remarks forMETAR:(METAR *)METAR {
     if (self = [super initFromRemarks:remarks forMETAR:METAR]) {
+        events = [NSMutableArray new];
+
         NSTextCheckingResult *match = [self matchRemarks:remarks withRegex:ThunderstormBeginEndRegex];
         if (!match) return (self = nil);
-        
-        self.begin = [self.parent parseDateFromMatch:match index:1 inString:remarks];
-        self.end = [self.parent parseDateFromMatch:match index:3 inString:remarks];
-        
+
+        ThunderstormEvent *event1 = [self parseEventFromMatch:match index:1 inString:remarks];
+        if (event1) [self.events addObject:event1];
+        ThunderstormEvent *event2 = [self parseEventFromMatch:match index:4 inString:remarks];
+        if (event2) [self.events addObject:event2];
+
         [remarks deleteCharactersInRange:match.range];
     }
     return self;
@@ -30,9 +58,45 @@ static NSString *ThunderstormBeginEndRegex = @"\\bTSB" REMARK_TIME_REGEX @"E" RE
 }
 
 - (NSString *) stringValue {
-    return [NSString localizedStringWithFormat:NSLocalizedString(@"thunderstorm began at %@ and ended at %@", @"remark"),
-            [self.parent.timeOnlyFormatter stringFromDate:[self.parent.calendar dateFromComponents:self.begin]],
-            [self.parent.timeOnlyFormatter stringFromDate:[self.parent.calendar dateFromComponents:self.end]]];
+    NSMutableArray *eventDescriptions = [NSMutableArray new];
+    [self.events enumerateObjectsUsingBlock:^(ThunderstormEvent *event, NSUInteger eventIndex, BOOL *stop) {
+        NSString *description;
+        switch (event.type) {
+            case ThunderstormEventTypeBegan:
+                if (eventIndex == 0)
+                    description = [NSString localizedStringWithFormat:NSLocalizedString(@"began at %@", @"thunderstorm event"),
+                                                  [self.parent.timeOnlyFormatter stringFromDate:[self.parent.calendar dateFromComponents:event.date]]];
+                else
+                    description = [NSString localizedStringWithFormat:NSLocalizedString(@"began again at %@", @"thunderstorm event"),
+                                   [self.parent.timeOnlyFormatter stringFromDate:[self.parent.calendar dateFromComponents:event.date]]];
+                break;
+            case ThunderstormEventTypeEnded:
+                description = [NSString localizedStringWithFormat:NSLocalizedString(@"ended at %@", @"thunderstorm event"),
+                               [self.parent.timeOnlyFormatter stringFromDate:[self.parent.calendar dateFromComponents:event.date]]];
+                break;
+        }
+        [eventDescriptions addObject:description];
+    }];
+
+    NSString *combinedDescriptions = [eventDescriptions componentsJoinedByString:NSLocalizedString(@" and ", @"list joiner")];
+    return [NSString localizedStringWithFormat:NSLocalizedString(@"thunderstorms %@", @"remark"), combinedDescriptions];
+}
+
+@end
+
+@implementation ThunderstormBeginEndRemark (Private)
+
+- (ThunderstormEvent *)parseEventFromMatch:(NSTextCheckingResult *)match index:(NSUInteger)index inString:(NSString *)remarks {
+    ThunderstormEvent *event = [ThunderstormEvent new];
+
+    NSString *eventTypeString = [remarks substringWithRange:[match rangeAtIndex:index]];
+    if ([eventTypeString isEqualToString:@"B"]) event.type = ThunderstormEventTypeBegan;
+    else if ([eventTypeString isEqualToString:@"E"]) event.type = ThunderstormEventTypeEnded;
+    else return nil;
+
+    event.date = [self.parent parseDateFromMatch:match index:index+1 inString:remarks];
+
+    return event;
 }
 
 @end
